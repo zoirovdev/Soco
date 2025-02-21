7f84d702254fe118a20e27d70c83aeeb77f94dbc
zoirovdev <abboszoirov17@gmail.com>
2025-02-21 21:06:46 +0000

    Fix bugs

--- main.c
+++ main.c
@@ -1,51 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <time.h>
+#include <locale.h>
 
 #define DEMON 'X'
 #define PACMAN 'C'
 #define FOOD '.'
-#define WALL '#'
+#define OUTER_WALL '#'
+#define INNER_WALL '='
 #define EMPTY ' '
 #define HEIGHT 20
 #define WIDTH 40
 
 
+
 char board[HEIGHT][WIDTH];
-bool isnotover;
+bool isgameover;
+int score;
 
 void put_walls(){
 	//putting outer walls
 	for(int i=0; i<HEIGHT; i++){
 		for(int j=0; j<WIDTH; j++){
 			if(i == 0 || j == 0 || i == HEIGHT - 1 || j == WIDTH - 1){
-				board[i][j] = WALL;
+				board[i][j] = OUTER_WALL;
 			} else {
 				board[i][j] = EMPTY;
 			}
 		}
-		printf("\n");
 	}
 
 	//putting inner walls
 	int count = 50;
 	while(count--){
-		int i = (rand() % (HEIGHT + 1));
-		int j = (rand() % (WIDTH + 1));
+		int y = rand() % HEIGHT + 1;
+		int x = rand() % WIDTH + 1;
 
-		if(board[i][j] != WALL && board[i][j] != PACMAN){
-			board[i][j] = WALL;
-		}
-	}
+		int rand_nums[] = {-1, 0, 1};
+		int idx = 0;
 
-	int val=5;
-	while(val--){
-		int row = (rand() % (HEIGHT + 1));
-		for(int j=3; j<WIDTH-3; j++){
-			if(board[row][j] != WALL && board[row][j] != PACMAN){
-				board[row][j] = WALL;
+		int rand_lengths[] = {1,3,5,7,9};
+		int length_idx = rand() % 5;
+		int length = rand_lengths[length_idx];
+
+		while(length--){
+			idx = rand() % 3;
+			if(idx==1){
+				x += rand_nums[idx];
+				y += rand_nums[idx];
+			} else if(idx==0){
+				y += rand_nums[idx];
+			} else if(idx==2){
+				x += rand_nums[idx];
 			}
+
+			if(board[y][x]!=OUTER_WALL){
+				board[y][x]=INNER_WALL;
+			}	
 		}
 	}
 }
@@ -57,7 +69,7 @@ void put_demons(){
 		int i = rand() % HEIGHT;
 	       	int j = rand() % WIDTH;
 
-		if(board[i][j]!=WALL && board[i][j]!=PACMAN){
+		if(board[i][j]!=OUTER_WALL && board[i][j]!=INNER_WALL){
 			board[i][j] = DEMON;
 		}
 	}
@@ -67,7 +79,7 @@ void put_demons(){
 void put_foods(){
 	for(int i=0; i<HEIGHT; i++){
 		for(int j=0; j<WIDTH; j++){
-			if(board[i][j]!=WALL && board[i][j]!=PACMAN && board[i][j]!=DEMON){
+			if(board[i][j]!=OUTER_WALL && board[i][j]!=INNER_WALL && board[i][j]!=DEMON){
 				board[i][j] = FOOD;
 			}
 		}
@@ -81,7 +93,7 @@ void put_pacman(){
 		int i = rand() % HEIGHT;
 		int j = rand() % WIDTH;
 
-		if(board[i][j] != WALL && board[i][j] != DEMON){
+		if(board[i][j]!=OUTER_WALL && board[i][j]!=DEMON && board[i][j]!=INNER_WALL){
 			board[i][j] = PACMAN;
 			break;
 		}
@@ -115,26 +127,34 @@ void move(int x, int y){
 	int dest_x = pacman_x + x;
 	int dest_y = pacman_y + y;
 	
-	if(board[dest_y][dest_x]==DEMON){
-		isnotover = false;
-	} else if(board[dest_y][dest_x]==FOOD || board[dest_y][dest_x]==EMPTY){
+	if(board[dest_y][dest_x] == DEMON){
+		printf("\nYou reached <%d scores>\n", score);
+		isgameover = false;
+	} else if(board[dest_y][dest_x] == FOOD){
+		board[pacman_y][pacman_x] = EMPTY;
+		board[dest_y][dest_x] = PACMAN;
+		score++;
+	} else if(board[dest_y][dest_x] == EMPTY){
 		board[pacman_y][pacman_x] = EMPTY;
 		board[dest_y][dest_x] = PACMAN;
 	}
 }
 					
 int main(){
+	srand(time(NULL));
 	put_walls();
 	put_demons();
 	put_foods();
 	put_pacman();
 	
-	isnotover = true;
-	while(isnotover){
+	score = 0;
+	isgameover = true;
+	while(isgameover){
 
 		system("clear");
 		draw();	
-		
+		printf("\nSCORE -: %d\n", score);
+
 		char c = getchar();
 		switch(c){
 			case 'a':
@@ -150,8 +170,9 @@ int main(){
 				move(0, -1);
 				break;
 			case 'q':
+				printf("\nYou reached <%d scores>\n", score);
 				printf("Quit\n");
-				isnotover = false;
+				isgameover = false;
 				break;
 			}
 	}
